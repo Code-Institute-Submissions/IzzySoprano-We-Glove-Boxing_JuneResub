3ac7d3f31bd9167219caee81cb2fa47b5547901c
IzzySoprano <Zerezghi@live.co.uk>
2022-02-24 13:48:46 +0000

    Added Commentary

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
+    "Creates category in database"
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, blank=True)
 
@@ -16,7 +17,10 @@ class Category(models.Model):
         return self.friendly_name
     
 class Product(models.Model):
+    "Creates products in database"
+
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    subcategory = models.ForeignKey('SubCategory', null=True, blank=True, on_delete=models.SET_NULL)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     sku = models.CharField(max_length=254, null=True, blank=True) 
     name = models.CharField(max_length=254)
@@ -27,6 +31,8 @@ class Product(models.Model):
         return self.name
 
 class SubCategory(models.Model):
+    "Model SubCategory allows the grouping of products for easier searches by sub-category of product"
+
     name = models.CharField(max_length=254)
     display_name = models.CharField(max_length=254)
 
