6754fe10c15d3c67d0b7543e449938db47dd2810
IzzySoprano <Zerezghi@live.co.uk>
2022-02-24 13:49:27 +0000

    Added category filtering

--- products/views.py
+++ products/views.py
@@ -1,31 +1,44 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category, SubCategory
 
 # Create your views here.
-
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ View to return all products, and sorting and searching """
 
     products = Product.objects.all()
     query = None
+    categories = None
+    SubCategory = None
+    subcategory = None
 
     if request.GET:
+        if 'Category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+
+        if 'sub_categories' in request.GET:
+            subcategory = request.GET['sub_categories']
+            products = products.filter(subcategory__name=subcategory)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(request, "You didn't enter anything!")
                 return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(product_description__icontains=query)
+
+            queries = (
+                Q(name__icontains=query) | Q(product_description__icontains=query)
+            )
             products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_subcategory': subcategory
     }
-
 
     return render(request, 'products/products.html', context)
 
